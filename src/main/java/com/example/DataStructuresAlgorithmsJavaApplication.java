673c58576d95ceca82f5632afdf930f71ca14965
“sg-miliyaz <santhosh.kuttamparambath@thinknsolutions.com>
2024-06-03 17:02:59 +0530

    Intial Commit

---
+++ src/main/java/com/example/DataStructuresAlgorithmsJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStructuresAlgorithmsJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStructuresAlgorithmsJavaApplication.class, args);
+	}
+
+}
